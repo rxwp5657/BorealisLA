9b05c0f24f2fd1976c523ed4842759a96d7ac44a
Juan Carlos Sanchez Ruiz de Chavec <RXWP5657@gmail.com>
2021-01-01 23:13:37 -0600

    Adding matrix test structure

---
+++ tests/matrix.test.ts
@@ -0,0 +1,34 @@
+describe("Matix test suite", () => {
+
+    it("should retrieve an entry of a matrix", () => {
+
+    })
+
+    it("should retrieve the size of a matrix as a tuple [m, n]", () => {
+
+    })
+
+    it("should transpose a matrix", () => {
+
+    })
+
+    it("should add two matrices of the same size", () => {
+
+    })
+
+    it("should substract two matrices of the same size", () => {
+
+    })
+
+    it("should multiply two matrices of the same size", () => {
+
+    })
+
+    it("should calculate a matrix determinant", () => {
+
+    })
+
+    it("should get the identity matrix", () => {
+
+    })
+})
